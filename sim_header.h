0458bfa54f48eb0a7ca4e16d7d5d51159497980e
Himesh Sharma <himesh.sharma1937@gmail.com>
2025-11-08 04:28:07 -0700

    Refactor include guards in sim_header.h
    Updated include guards to use 'SIM_HEADER_H'. Removed unnecessary comments.

--- sim_header.h
+++ sim_header.h
@@ -1,9 +1,6 @@
-#ifndef EX7Q1_H
-#define EX7Q1_H
+#ifndef SIM_HEADER_H
+#define SIM_HEADER_H
 
-/* DO NOT MODIFY THIS FILE */
-
-/* TODO: Implement this function in `ex7q1.c` */
 void salesperson_report(int player, int x, int y, int *n, int *e, int *s, int *w);
 
 #endif
